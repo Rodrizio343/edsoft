ef29cbe074bff8d3fb4476eadb3ff5741f989b18
Rodrigo <rodrigo.martinez@webexport.com.ar>
2023-05-26 00:48:18 +0100

    Service component

--- components/Home/Services.jsx
+++ components/Home/Services.jsx
@@ -1,23 +1,19 @@
-const Services = () => {
+import Service from "./Service"
+
+const Services = ({ services }) => {
   return (
     <section className="services-section section-container">
       <h2>Nuestros servicios</h2>
       <div className="content-wrapper">
-        <article className="service-item">
-          <span className="jam jam-phone"></span>
-          <h3>Servicio 1</h3>
-          <p>Diseñamos tu presencia en internet con apariencia profesional</p>
-        </article>
-        <article className="service-item">
-          <span className="jam jam-computer"></span>
-          <h3>Servicio 2</h3>
-          <p>Diseñamos tu presencia en internet con apariencia profesional</p>
-        </article>
-        <article className="service-item">
-          <span className="jam jam-keyboard"></span>
-          <h3>Servicio 3</h3>
-          <p>Diseñamos tu presencia en internet con apariencia profesional</p>
-        </article>
+        {
+          services && services.map( service => (
+            <Service
+              key={service.id}
+              name={service.name}
+              detail={service.detail}
+            />
+          ))
+        }
       </div>
     </section>
   )
--- pages/index.jsx
+++ pages/index.jsx
@@ -5,14 +5,26 @@ import Projects from '@/components/Home/Projects'
 import Services from '@/components/Home/Services'
 
 
-export default function Home() {
+export default function Home({services}) {
   return (
     <main>
       <Banner />
-      <Services />
+      <Services services={services} />
       <AboutUs />
       <Projects />
       <Jobs />
     </main>
   )
+}
+
+export async function getStaticProps() {
+  
+  const resp = await fetch('http://localhost:4050/services');
+  const services = await resp.json();
+  
+  return {
+    props: {
+      services
+    }
+  }
 }
