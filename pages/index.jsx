4ed6b6ab5bfc8ccc20d022e199ff1b246a82fcad
Rodrigo <rodrigo.martinez@webexport.com.ar>
2023-05-26 02:13:37 +0100

    Title tags

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,17 +3,23 @@ import Banner from '@/components/Home/Banner'
 import Jobs from '@/components/Home/Jobs'
 import Projects from '@/components/Home/Projects'
 import Services from '@/components/Home/Services'
+import Head from 'next/head'
 
 
 export default function Home({services, projects}) {
   return (
-    <main>
-      <Banner />
-      <Services services={services} />
-      <AboutUs />
-      <Projects projects={projects}/>
-      <Jobs />
-    </main>
+    <>
+      <Head>
+        <title>Inicio</title>
+      </Head>
+      <main>
+        <Banner />
+        <Services services={services} />
+        <AboutUs />
+        <Projects projects={projects}/>
+        <Jobs />
+      </main>
+    </>
   )
 }
 
--- pages/proyectos/index.jsx
+++ pages/proyectos/index.jsx
@@ -1,17 +1,23 @@
 import Project from '@/components/Projects/Project';
+import Head from 'next/head';
 
 const Projects = ({ projects }) => {
   return (
-    <section className="projects-section section-container">
-      <h1>Nuestros Proyectos</h1>
-      <div className="content-wrapper">
-        {
-          projects && projects.map(project => (
-            <Project key={project.id} project= {project}/>
-          ))
-        }
-      </div>
-    </section>
+    <>
+      <Head>
+        <title>Proyectos</title>
+      </Head>
+      <section className="projects-section section-container">
+        <h1>Nuestros Proyectos</h1>
+        <div className="content-wrapper">
+          {
+            projects && projects.map(project => (
+              <Project key={project.id} project= {project}/>
+            ))
+          }
+        </div>
+      </section>
+    </>
   )
 }
 
